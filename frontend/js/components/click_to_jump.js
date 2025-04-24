2a774d52e67b887314a3845e061fddf15afc9ed4
Xudong Zhao <info.xd.ovo@gmail.com>
2025-04-24 23:09:05 +0800

    Demo design: how to reuse header code

--- frontend/js/components/click_to_jump.js
+++ frontend/js/components/click_to_jump.js
@@ -1,4 +1,4 @@
 // click the logo to jump to the index page
 document.getElementById('logoImage').addEventListener('click', function() {
-    window.location.href = '/frontend/index.html';
+    window.location.href = '/index.html';
 });
